fda50b3263b2cc33fcc3a4f5945edbb51febf1a7
Noah Scott <noahleonardscott@gmail.com>
2024-03-15 11:53:14 -0700

    Intial commit

---
+++ Database.h
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.h to edit this template
+ */
+
+/* 
+ * File:   Database.h
+ * Author: NoahP
+ *
+ * Created on March 15, 2024, 11:43 AM
+ */
+
+#ifndef DATABASE_H
+#define DATABASE_H
+
+class Database {
+public:
+    Database();
+    Database(const Database& orig);
+    virtual ~Database();
+private:
+
+};
+
+#endif /* DATABASE_H */
+
---
+++ main.cpp
@@ -0,0 +1,21 @@
+
+
+/* 
+ * File:   main.cpp
+ * Author: NoahP
+ *
+ * Created on March 15, 2024, 11:32 AM
+ */
+#include <iostream>
+#include "Database.h"
+
+using namespace std;
+
+int main (){
+    Database data;
+    
+    cout << "hello world" << endl;
+    
+    return 0;
+}
+
